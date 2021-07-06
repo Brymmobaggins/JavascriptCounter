668eaeb5234e266309e3bf272ca5286b5e8e1c24
Brymmobaggins <ibro2tablet21@yahoo.com>
2021-07-06 17:23:00 +0100

    fix js

---
+++ main.js
@@ -0,0 +1,9 @@
+let add = document.getElementById('increment')
+let decrease = document.getElementById('decrement')
+
+let int = document.getElementById('number');
+let integer = 0;
+
+addEventListener('click', function () {
+    integer += 1;
+})
