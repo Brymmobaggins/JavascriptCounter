6d5f65034fcf7e5d1264fb9c368f3cbc3663a039
Brymmobaggins <ibro2tablet21@yahoo.com>
2021-07-07 13:16:49 +0100

    update task

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
 let add = document.getElementById('increment')
 let decrease = document.getElementById('decrement')
-
 let int = document.getElementById('number');
+
 let integer = 0;
 
 add.addEventListener('click', function () {
-    integer += 1;
+    integer++;
     int.innerHTML = integer;
+    if (int === 50) {
+        integer = 0;
+    }
+    // else {
+    //     int = 0;
+    // }
 })
 
-decrease.addEventListener('click',function () {
-    integer -= 1;
+decrease.addEventListener('click', function () {
+    integer--;
     int.innerHTML = integer;
+    if (integer === 0) {
+        int = 0;
+    }
 
 })
